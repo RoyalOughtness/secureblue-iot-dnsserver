904763758fd7d7eb0cb23fe26df1927a038d67f9
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2026-02-28 21:12:25 -0800

    fix build

--- files/scripts/setdnscryptproxysettings.sh
+++ files/scripts/setdnscryptproxysettings.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
 
+set -euo pipefail
 
 sed -i \
-  "s/^# server_names = \['scaleway-fr', 'google', 'yandex', 'cloudflare'\]/server_names = ['cloudflare-security']/" \
+  -e "s/^# server_names = \['scaleway-fr', 'google', 'yandex', 'cloudflare'\]/server_names = ['cloudflare-security']/" \
   -e "s/^listen_addresses = \['127.0.0.1:53'\]/listen_addresses = ['0.0.0.0:5053']/" \
   /etc/dnscrypt-proxy/dnscrypt-proxy.toml
